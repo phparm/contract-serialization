b526c86e5eeb35c8f9f26f64ca6234afeacbe7ce
DengHuiJie <jaycedeng@outlook.com>
2025-11-20 18:30:14 +0800

    add contract

---
+++ src/Jsonable.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phparm\Contract\Support;
+
+interface Jsonable
+{
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson(int $options = 0): string;
+}
